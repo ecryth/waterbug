a13a8368522037bf9286ca6360298515fc481885
ecryth <885076+ecryth@users.noreply.github.com>
2011-10-30 14:52:20 +0100

    Finished %anidb command

--- src/waterbug/modules/anidb.py
+++ src/waterbug/modules/anidb.py
@@ -1,5 +1,7 @@
 
 import pprint
+import gzip
+import urllib.request
 import xml.etree.ElementTree as ElementTree
 
 import waterbug.waterbug as waterbug
@@ -8,47 +10,73 @@ class Commands:
     
     class AnidbCommands:
         
-#        class TitleHandler(xml.sax.handler.ContentHandler):
-#            
-#            def __init__(self):
-#                super().__init__()
-#                self.titles = {}
-#                self.currenttitle = None
-#            
-#            def startElement(self, name, attrs):
-#                if name == "anime":
-#                    self.currentanime = {}
-#                    self.titles[int(attrs.getValue("aid"))] = self.currentanime
-#                elif name == "title":
-#                    self.currenttitle = {"type": attrs.getValue("type"),
-#                                         "lang": attrs.getValue("xml:lang")}
-#            
-#            def endElement(self, name):
-#                if name == "title":
-#                    self.currenttitle = None
-#            
-#            def characters(self, content):
-#                if self.currenttitle is not None:
-#                    if self.currenttitle["type"] not in self.currentanime:
-#                        self.currentanime[self.currenttitle["type"]] = {}
-#                    if self.currenttitle["lang"] not in self.currentanime[self.currenttitle["type"]]:
-#                        self.currentanime[self.currenttitle["type"]][self.currenttitle["lang"]] = []
-#                    self.currentanime[self.currenttitle["type"]][self.currenttitle["lang"]] \
-#                                                    .append(content.strip())
-        
-        
         def __init__(self):
-            self.titles = {}
-            for (event, elem) in ElementTree.iterparse(open("animetitles.xml"), ("start", "end")):
+            self.titles = self.load_titles(open("animetitles.xml"))
+            self.cache = {}
+            self.url_info = {
+                    "server": "api.anidb.net",
+                    "port": 9001,
+                    "protoversion": 1,
+                    "clientname": "eldishttp",
+                    "clientversion": 1
+            }
+        
+        def load_titles(self, file):
+            titles = {}
+            for (event, elem) in ElementTree.iterparse(file, ("start", "end")):
                 if event == "start" and elem.tag == "anime":
                     currentanime = {}
-                    self.titles[int(elem.attrib['aid'])] = currentanime
+                    titles[int(elem.attrib['aid'])] = currentanime
                 if event == "end" and elem.tag == "title":
                     if elem.attrib['type'] not in currentanime:
                         currentanime[elem.attrib['type']] = {}
                     if elem.attrib['{http://www.w3.org/XML/1998/namespace}lang'] not in currentanime[elem.attrib['type']]:
                         currentanime[elem.attrib['type']][elem.attrib['{http://www.w3.org/XML/1998/namespace}lang']] = []
                     currentanime[elem.attrib['type']][elem.attrib['{http://www.w3.org/XML/1998/namespace}lang']].append(elem.text)
+            return titles
+        
+        def fetch_anime(self, aid):
+            if aid in self.cache:
+                return self.cache[aid]
+            
+            info = {}
+            info_file = gzip.GzipFile(fileobj=urllib.request.urlopen(
+                                        "http://{server}:{port}/httpapi?request=anime&client={clientname}"
+                                        "&clientver={clientversion}&protover={protoversion}&aid={aid}".format(
+                                                                                    aid=aid, **self.url_info)))
+            
+            root = ElementTree.parse(info_file)
+            info["type"] = root.find("type").text
+            info["episodecount"] = int(root.find("episodecount").text)
+            info["startdate"] = getattr(root.find("startdate"), "text", "???")
+            info["enddate"] = getattr(root.find("enddate"), "text", "???")
+            info["relatedanime"] = []
+            tmp = root.find("relatedanime")
+            if tmp is not None:
+                for anime in tmp:
+                    info["relatedanime"].append({"aid": int(anime.attrib['id']), "type": anime.attrib['type'],
+                                                 "title": anime.text})
+            info["similaranime"] = []
+            tmp = root.find("similaranime")
+            if tmp is not None:
+                for anime in tmp:
+                    info["similaranime"].append({"aid": int(anime.attrib['id']), "approval": int(anime.attrib['approval']),
+                                                 "total": int(anime.attrib['total']), "title": anime.text,
+                                                 "percentage": round(100*int(anime.attrib['approval'])/int(anime.attrib['total']), 2)})
+            info["similaranime"].sort(key=lambda x: x['percentage'], reverse=True)
+            info["categories"] = []
+            tmp = root.find("categories")
+            if tmp is not None:
+                info["categories"] = list(map(lambda x: {"name": x.find("name").text,
+                                                         "weight": int(x.attrib['weight'])},
+                                              sorted(list(tmp), key=lambda x: int(x.attrib['weight']),
+                                                     reverse=True)))
+            info["rating"] = getattr(root.find("ratings/permanent"), "text", "???")
+            
+            self.cache[aid] = info
+            
+            return info
+            
         
         def all_in(self, a, b):
             for i in a:
@@ -101,8 +129,12 @@ class Commands:
                 return
             
             aid, titles = next(iter(r.items()))
-            server.msg(data["target"], "{} - http://anidb.net/a{}".format(
-                                                            self.format_title(titles), aid))
+            info = self.fetch_anime(aid)
+            server.msg(data["target"], "{}, {}, aired {}, {} episode(s), rating: {}, [{}] - http://anidb.net/a{}" \
+                       .format(self.format_title(titles), info['type'],
+                               info['startdate'] if info['startdate'] == info['enddate'] else "{}­­–{}" \
+                                        .format(info['startdate'], info['enddate']), info['episodecount'],
+                               info['rating'], ", ".join(map(lambda x: x['name'], info['categories'][:9])), aid))
         
         @waterbug.expose()
         def add(self, data, server, *args):
