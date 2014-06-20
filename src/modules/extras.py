f39c2c742cedddd90afaba271d21cc6e956cf4b0
ecryth <885076+ecryth@users.noreply.github.com>
2014-06-20 13:05:34 +0200

    Add %translate

---
+++ src/modules/extras.py
@@ -0,0 +1,32 @@
+
+import asyncio
+import json
+import re
+import urllib.parse
+
+import waterbug
+
+parser = waterbug.ArgumentParser()
+parser.add_argument('-f', default="sv")
+parser.add_argument('-t', default="en")
+parser.add_argument('string', nargs='+')
+
+class Commands:
+
+    @waterbug.expose
+    @asyncio.coroutine
+    def translate(responder, *text):
+        qstring = urllib.parse.urlencode({
+            "client": "t",
+            "sl": ns.f,
+            "tl": ns.t,
+            "dt": "t",
+            "ie": "UTF-8",
+            "oe": "UTF-8",
+            "q": " ".join(ns.string)
+        })
+        body = yield from waterbug.fetch_url(
+            "https://translate.google.com/translate_a/single?" + qstring, method="GET",
+            headers={"User-Agent": ""})
+        res = json.loads(re.sub(',(?=,)', ',null', body.decode('utf-8')))
+        responder(res[0][0][0])
--- src/waterbug/bot.py
+++ src/waterbug/bot.py
@@ -14,7 +14,7 @@
 #   You should have received a copy of the GNU Affero General Public License
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-__all__ = ['Waterbug', 'expose', 'trigger']
+__all__ = ['Waterbug', 'ArgumentParser', 'expose', 'trigger']
 
 import argparse
 import asyncio
